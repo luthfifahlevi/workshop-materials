254132da9e5ca1da11612cb32f39eba2b727bd1e
muhammad.fahlevi <muhammad.fahlevi@gojek.com>
2025-10-26 21:27:08 +0700

    add gitignore

--- python/project/student_score_manager/storage.py
+++ python/project/student_score_manager/storage.py
@@ -3,12 +3,12 @@ from typing import List
 
 from student import Student
 
-def save_to_file(students: List[Student], path: str = "students.json") -> None:
+def save_to_file(students: List[Student], path: str = "python/project/student_score_manager/students.json") -> None:
     data = [s.to_dict() for s in students]
     with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
-def load_from_file(path: str = "students.json") -> List[Student]:
+def load_from_file(path: str = "python/project/student_score_manager/students.json") -> List[Student]:
     try:
         with open(path, "r", encoding="utf-8") as f:
             data = json.load(f)
